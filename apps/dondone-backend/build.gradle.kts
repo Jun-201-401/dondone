f8feb11a7e82d22e17401f94ad253e25e38acab3
강보승 <gda05189@gmail.com>
2026-03-10 10:14:23 +0900

    chore: Gradle Kotlin DSL import 위치 정리

--- apps/dondone-backend/build.gradle.kts
+++ apps/dondone-backend/build.gradle.kts
@@ -1,10 +1,10 @@
+import org.gradle.api.tasks.testing.Test
+
 plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
 }
-
-import org.gradle.api.tasks.testing.Test
 
 group = "com.workproofpay"
 version = "0.1.0-SNAPSHOT"
